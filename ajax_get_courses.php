5c6c5bc4fe720739c362508a1c5122276c611dea
HilquiasAbias <hilquias.dev@gmail.com>
2024-10-15 15:59:06 -0300

    feat: finalizando paginação e iniciando footer

--- ajax_get_courses.php
+++ ajax_get_courses.php
@@ -6,7 +6,15 @@ use core_course\course;
 
 $current_page = max(0, optional_param('page', 0, PARAM_INT));
 $courses_per_page = max(1, optional_param('limit', 8, PARAM_INT));
+$query = optional_param('q', '', PARAM_TEXT);
 
+// Nega requisições que não sejam internas
+$host = $_SERVER['HTTP_HOST'];
+$referer = isset($_SERVER['HTTP_REFERER']) ? parse_url($_SERVER['HTTP_REFERER']) : null;
+if (!$referer || $referer['host'] !== $host) {
+    echo json_encode(['error' => 'Access denied.']);
+    die;
+}
 
 $categories = $DB->get_records('course_categories', null, '', 'id, name');
 $category_names = [];
@@ -14,20 +22,36 @@ foreach ($categories as $category) {
     $category_names[$category->id] = $category->name;
 }
 
-$sql = "
-    SELECT id, fullname, category
-    FROM {course}
-    WHERE visible = 1 AND id != 1
-    ORDER BY id
-    LIMIT :limit OFFSET :offset
-";
-$params = [
-    'limit' => $courses_per_page,
-    'offset' => $current_page * $courses_per_page
-];
-$courses = $DB->get_records_sql($sql, $params);
+if (!empty($query)) {
+    $sql = "
+        SELECT id, fullname, category
+        FROM {course}
+        WHERE visible = 1 AND id != 1 AND fullname LIKE :query
+        ORDER BY id
+        LIMIT :limit OFFSET :offset
+    ";
+    $params = [
+        'query' => "%{$query}%",
+        'limit' => $courses_per_page,
+        'offset' => $current_page * $courses_per_page
+    ];
+    $size = $DB->count_records('course', ['visible' => 1, 'fullname' => $query]) - 1;
+} else {
+    $sql = "
+        SELECT id, fullname, category
+        FROM {course}
+        WHERE visible = 1 AND id != 1
+        ORDER BY id
+        LIMIT :limit OFFSET :offset
+    ";
+    $params = [
+        'limit' => $courses_per_page,
+        'offset' => $current_page * $courses_per_page
+    ];
+    $size = $DB->count_records('course', ['visible' => 1]) - 1;
+}
 
-$size = $DB->count_records('course', ['visible' => 1]) - 1;
+$courses = $DB->get_records_sql($sql, $params);
 
 $courses_response = [];
 foreach ($courses as $course) {
@@ -44,8 +68,5 @@ foreach ($courses as $course) {
     $courses_response[] = $course_response;
 }
 
-// TODO: retornar a quantidade total de cursos {total: $size, courses: $courses_response}
-// TODO: atualizar o front-end para desabilitar o botão de "Próximo" quando não houver mais cursos
-
-echo json_encode($courses_response);
+echo json_encode(['total' => $size, 'courses' => $courses_response]);
 die;
